c24dae82744b85e66e72219c93fe8ae44750d4f0
dhopz <david_houpapa@hotmail.com>
2021-12-01 12:24:08 +0000

    adding market data

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -1,4 +1,4 @@
-import { makeStyles, Typography } from '@material-ui/core';
+import { LinearProgress, makeStyles, Typography } from '@material-ui/core';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Classnames } from 'react-alice-carousel';
@@ -7,6 +7,7 @@ import CoinInfo from '../components/CoinInfo';
 import { SingleCoin } from '../config/api';
 import { CryptoState } from '../CryptoContext';
 import ReactHtmlParser from 'react-html-parser'
+import { numberWithCommas } from '../components/CoinsTable';
 
 const CoinPage = () => {
   const { id } = useParams();
@@ -47,14 +48,41 @@ const CoinPage = () => {
     },
     heading:{
       flexGrow: 1,
-      color:"#00ADB5",
+      color:"#00FFF5",
       fontFamily:"Roboto",
       fontWeight:"bold",
       marginBottom:20, 
+    },
+    description:{
+      width:"100%",
+      fontFamily:"Roboto",
+      padding:25,
+      paddingBottom:15,
+      paddingTop:0,
+      textAlign:"justify",
+    },
+    marketData: {
+      alignSelf: "start",
+      padding: 25,
+      paddingTop: 10,
+      width: "100%",
+      [theme.breakpoints.down("md")]: {
+        display: "flex",
+        justifyContent: "space-around",
+      },
+      [theme.breakpoints.down("sm")]: {
+        flexDirection: "column",
+        alignItems: "center",
+      },
+      [theme.breakpoints.down("xs")]: {
+        alignItems: "start",
+      },
     }
     
   }));
   const classes = useStyles();
+
+  if (!coin) return <LinearProgress style={{backgroundColor:"#00FFF5"}}/>
 
       return (
         <div className = {classes.container}>
@@ -71,6 +99,57 @@ const CoinPage = () => {
             <Typography variant="subtitle1" className={classes.description}>
               {ReactHtmlParser(coin?.description.en.split(". ")[0])}.
             </Typography>
+            <div className={classes.marketData}>
+              <span style={{display:"flex"}}>
+                <Typography variant="h5" className={classes.heading}>
+                  Rank:
+                </Typography>
+                &nbsp;&nbsp;
+                <Typography
+                variant="h5"
+                style={{
+                  fontFamily:"Roboto", 
+                }}>
+                  {coin?.market_cap_rank}
+                </Typography>
+              </span>
+              <span style={{display:"flex"}}>
+                <Typography variant="h5" className={classes.heading}>
+                  Current Price:
+                </Typography>
+                
+                <Typography
+                variant="h5"
+                style={{
+                  fontFamily:"Roboto", 
+                }}
+                >
+                  {symbol}{" "}
+                  {numberWithCommas(
+                    coin?.market_data.current_price[currency.toLowerCase()]
+                  )}
+                </Typography>
+              </span>
+              <span style={{display:"flex"}}>
+                <Typography variant="h5" className={classes.heading}>
+                  Market Cap:
+                </Typography>
+                &nbsp;&nbsp;
+                <Typography
+                variant="h5"
+                style={{
+                  fontFamily:"Roboto", 
+                }}
+                >
+                  {symbol}{" "}
+                  {numberWithCommas(
+                    coin?.market_data.market_cap.[currency.toLowerCase()]
+                      .toString()
+                      .slice(0, -6))}M
+                </Typography>
+              </span>
+              
+            </div>
 
 
           </div>
