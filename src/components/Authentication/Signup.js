e02d61fb8123651994fe18eed23dffe5d215a08c
dhopz <david_houpapa@hotmail.com>
2021-12-02 14:57:42 +0000

    signup page created

--- src/components/Authentication/Signup.js
+++ src/components/Authentication/Signup.js
@@ -1,4 +1,4 @@
-import { Box } from '@material-ui/core'
+import { Box, Button, TextField } from '@material-ui/core'
 import React, { useState } from 'react'
 
 const Signup = ({handleClose}) => {
@@ -7,11 +7,47 @@ const Signup = ({handleClose}) => {
     const [password, setPassword] = useState("")
     const [confirmPassword, setconfirmPassword] = useState("")
 
+    const handleSubmit = () => {}
+
 
     return (
         <Box 
         p={3}
         style={{display:"flex", flexDirection:"column", gap:"20px"}}
+        >
+        <TextField
+            variant="outlined"
+            type="Email"
+            label="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            fullWidth
+        />
+        <TextField
+            variant="outlined"
+            type="Password"
+            label="Enter Password"
+            value={password}
+            onChange={(e) => setEmail(e.target.value)}
+            fullWidth
+        />
+        <TextField
+            variant="outlined"
+            type="Password"
+            label="Enter Password"
+            value={confirmPassword}
+            onChange={(e) => setEmail(e.target.value)}
+            fullWidth
+        />
+        <Button
+        variant="contained"
+        size="large"
+        style={{backgroundColor:"#00FFF5"}}
+        onClick={handleSubmit}
+        >
+        Sign Up
+        </Button>  
+
 
         </Box>
     )
