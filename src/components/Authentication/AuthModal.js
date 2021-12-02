5b20d7a0e07990555a5931d5a564596cff602162
dhopz <david_houpapa@hotmail.com>
2021-12-02 14:02:45 +0000

    tab modal set up

--- src/components/Authentication/AuthModal.js
+++ src/components/Authentication/AuthModal.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
+import { AppBar, Button, Tab, Tabs } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -30,11 +31,27 @@ export default function AuthModal() {
     setOpen(false);
   };
 
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+      setValue(newValue);
+  }
+    
+
   return (
     <div>
-      <button type="button" onClick={handleOpen}>
-        react-transition-group
-      </button>
+      <Button 
+      variant="contained"
+      style={{
+          width:85,
+          height:40,
+          marginLeft:15,
+          backgroundColor:"#00FFF5"
+      }}
+      onClick={handleOpen}
+      >
+          Login
+      </Button>
       <Modal
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
@@ -49,8 +66,20 @@ export default function AuthModal() {
       >
         <Fade in={open}>
           <div className={classes.paper}>
-            <h2 id="transition-modal-title">Transition modal</h2>
-            <p id="transition-modal-description">react-transition-group animates me.</p>
+              <AppBar 
+              position="static"
+              style= {{backgroundColor:"transparent", color:"white"}}>
+                  <Tabs 
+                  value={value} 
+                  onChange={handleChange}
+                  variant="fullWidth"
+                  style={{borderRadius:10}}
+                  >
+                      <Tab label="Login"/>
+                      <Tab label="Sign Up"/>
+                  </Tabs>
+              </AppBar>
+            
           </div>
         </Fade>
       </Modal>
