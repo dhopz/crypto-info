126f0e7ac5c6e0b59f685ffbd4c3617e62261733
dhopz <david_houpapa@hotmail.com>
2021-12-02 16:52:39 +0000

    login with snackbar message

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -1,11 +1,44 @@
+import { getAuth, signInWithEmailAndPassword } from '@firebase/auth'
 import { Box, Button, TextField } from '@material-ui/core'
 import React, { useState } from 'react'
+import { CryptoState } from '../../CryptoContext'
+
 
 const Login = ({handleClose}) => {
 
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const handleSubmit = () => {}
+    const { setAlert } =CryptoState()
+    const auth = getAuth();
+
+    const handleSubmit = async () => {
+        if (!email || !password) {
+          setAlert({
+            open: true,
+            message: "Please fill all the Fields",
+            type: "error",
+          });
+          return;
+        }
+    
+        try {
+          const result = await signInWithEmailAndPassword(auth, email, password);
+          setAlert({
+            open: true,
+            message: `Sign Up Successful. Welcome ${result.user.email}`,
+            type: "success",
+          });
+    
+          handleClose();
+        } catch (error) {
+          setAlert({
+            open: true,
+            message: error.message,
+            type: "error",
+          });
+          return;
+        }
+      };
 
     return (
         <Box 
