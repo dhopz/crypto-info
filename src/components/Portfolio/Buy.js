c9996748c404805e0b0258c8586e88e934de4648
dhopz <david_houpapa@hotmail.com>
2021-12-08 17:40:19 +0000

    tab added

---
+++ src/components/Portfolio/Buy.js
@@ -0,0 +1,75 @@
+import { Box, Button, TextField } from '@material-ui/core'
+import React, { useState } from 'react'
+import { CryptoState } from '../../CryptoContext'
+
+
+const Buy = ({handleClose}) => {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const { setAlert } =CryptoState()
+    const auth = getAuth();
+    
+    const handleSubmit = async () => {
+        if (!email || !password) {
+          setAlert({
+            open: true,
+            message: "Please fill all the Fields",
+            type: "error",
+          });
+          return;
+        }
+    
+        try {
+          const result = await signInWithEmailAndPassword(auth, email, password);
+          setAlert({
+            open: true,
+            message: `Sign Up Successful. Welcome ${result.user.email}`,
+            type: "success",
+          });
+          // console.log(result);
+          handleClose();
+        } catch (error) {
+          setAlert({
+            open: true,
+            message: error.message,
+            type: "error",
+          });
+          return;
+        }
+      };
+
+    return (
+        <Box 
+        p={3}
+        style={{display:"flex", flexDirection:"column", gap:"20px"}}
+        >
+        <TextField
+            variant="outlined"
+            type="Email"
+            label="Enter Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            fullWidth
+        />
+        <TextField
+            variant="outlined"
+            type="Password"
+            label="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            fullWidth
+        />
+        <Button
+        variant="contained"
+        size="large"
+        style={{backgroundColor:"#00FFF5"}}
+        onClick={handleSubmit}
+        >
+        Login
+        </Button> 
+        </Box>
+    )
+}
+
+export default Buy
--- src/components/Portfolio/PortInstruct.js
+++ src/components/Portfolio/PortInstruct.js
@@ -1,85 +1,78 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-    align: "center",
-    justify:"center",
-    alignItems: "center",
-  },
-  selectEmpty: {
-    marginTop: theme.spacing(2),
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
   },
 }));
 
-export default function SimpleSelect() {
+export default function SimpleTabs() {
   const classes = useStyles();
-  const [age, setAge] = React.useState('');
+  const [value, setValue] = React.useState(0);
 
-  const handleChange = (event) => {
-    setAge(event.target.value);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
   };
 
+  
+
   return (
-    <div>
-      <FormControl className={classes.formControl}>
-        <InputLabel id="demo-simple-select-helper-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-helper-label"
-          id="demo-simple-select-helper"
-          value={age}
-          onChange={handleChange}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-        <FormHelperText>Some important helper text</FormHelperText>
-      </FormControl>
-      <FormControl className={classes.formControl}>
-        <InputLabel id="demo-simple-select-helper-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-helper-label"
-          id="demo-simple-select-helper"
-          value={age}
-          onChange={handleChange}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-        <FormHelperText>Some important helper text</FormHelperText>
-      </FormControl>
-      <FormControl className={classes.formControl}>
-        <InputLabel id="demo-simple-select-helper-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-helper-label"
-          id="demo-simple-select-helper"
-          value={age}
-          onChange={handleChange}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-        <FormHelperText>Some important helper text</FormHelperText>
-      </FormControl>
-    </div>
+    <div className={classes.paper}>
+              <AppBar 
+              position="static"
+              style= {{backgroundColor:"transparent", color:"white"}}>
+                  <Tabs 
+                  value={value} 
+                  onChange={handleChange}
+                  variant="fullWidth"
+                  style={{borderRadius:10}}
+                  >
+                      <Tab label="Buy"/>
+                      <Tab label="Sell"/>
+                  </Tabs>
+              </AppBar> 
+
+          </div>
   );
 }
